aba46f9053aa9f8ecafdd972cdda3690008a14a9
Steve Helmerich <16daystocode@gmail.com>
2020-06-23 15:27:31 -0400

    gets and sets in Inventory.java

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,6 +1,33 @@
+
+
 public class Inventory {
+
+    String[][] backpack = new String[3][9];
+    String[] hotbar = new String[9];
 
     public Inventory() {
 
+    }
+
+    public String getItemInHotbar(int pos) {
+        return hotbar[pos];
+    }
+
+    public String getItemInBackpack(int row, int col) {
+        return backpack[row][col];
+    }
+
+    public void setItemInHotbar(int pos, String item) {
+        hotbar[pos] = item;
+    }
+
+    public void setItemInBackpack(int row, int col, String item) {
+        backpack[row][col] = item;
+    }
+
+    public void setBackpack(String[][] newBP) {
+        if(newBP.length == 3 && newBP[0].length == 9) {
+            backpack = newBP;
+        }
     }
 }
